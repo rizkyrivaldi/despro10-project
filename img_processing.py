0a7a9b32f1128ebeb9f4da9d6c5b964ce92cd83b
Kofrom506 <71697178+Kofrom506@users.noreply.github.com>
2023-11-07 20:27:20 +0700

    firebase push

--- img_processing.py
+++ img_processing.py
@@ -1,15 +1,25 @@
 import cv2
 import os
+import firebase_admin
+from firebase_admin import credentials
+import schedule
+import time
+import datetime
+
+cred = credentials.Certificate("/Users/evansusanto/Documents/Python Despro/despro10-project/esp32firebase-41756-firebase-adminsdk-656eb-3be1b739d0.json")
+firebase_admin.initialize_app(cred)
 
 # Yolo Paths
 model_path = "model/yolov4.weights"
 cfg_path =  "model/yolov4.cfg"
 classes_path = "model/classes.names"
 
+db = firestore.client()
+
 # Frame Paths
 # image_path = "assets/street.jpg"
 # detected_path = "assets/street_detected.jpg"
-image_path = "assets/snap.jpg"
+image_path = "assets/____snap.jpg"
 detected_path = "assets/snap_detected.jpg"
 
 print(os.getcwd())
@@ -36,20 +46,42 @@ class_ids, confidences, boxes = model.detect(frame, conv_threshold, nms_threshol
 
 indexes = range(0, len(list(class_ids)))
 
-person_counter = 0
-if len(list(class_ids)) > 0:
-    for i in indexes:
-        x,y,w,h = boxes[i]
-        label = str(classes[class_ids[i]])
-        confidence = str(round(confidences[i],2))
-        color = (0, 255, 0)
-        cv2.rectangle(frame,(x,y),(x+w,y+h),color,2)
-        cv2.putText(frame,label + " " + confidence, (x,y-10),font,1,color,1)
-        if class_ids[i] == 0:
-            person_counter += 1
+def getPersonCount():
+    person_counter = 0
+    if len(list(class_ids)) > 0:
+        for i in indexes:
+            x,y,w,h = boxes[i]
+            label = str(classes[class_ids[i]])
+            confidence = str(round(confidences[i],2))
+            color = (0, 255, 0)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),color,2)
+            cv2.putText(frame,label + " " + confidence, (x,y-10),font,1,color,1)
+            if class_ids[i] == 0:
+                person_counter += 1
+    return person_counter
         
 
 cv2.imwrite(detected_path, frame)
+
+def send_data_to_firestore():
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    collection_ref = db.collection('abc')
+    doc_ref = collection_ref.document('test')
+    data = {
+    'personCount': getPersonCount(),
+    'time': formatted_time
+    }
+    print("Send Data to Firestore {data}")
+    doc_ref.set(data)
+
+schedule.every(5).seconds.do(send_data_to_firestore)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+print("Data posted to Firestore.")
 
 print(class_ids)
 print(confidences)
